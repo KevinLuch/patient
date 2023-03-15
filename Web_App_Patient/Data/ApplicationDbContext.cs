b239024eb5fc65c17707ead3989795e06c558a76
Kevin <kevinluch@gmail.com>
2023-03-14 21:23:01 -0500

    first commit

---
+++ Web_App_Patient/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_App_Patient.Model;
+
+namespace Web_App_Patient.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public DbSet<Patient> Patient { get; set; }
+    }
+}
---
+++ Web_App_Patient/Model/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web_App_Patient.Model
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [DisplayName("Zip Code")]
+        public int ZipCode { get; set; }
+        [Required]
+        [DisplayName("Admit To")]
+        public string AdmitTo { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
