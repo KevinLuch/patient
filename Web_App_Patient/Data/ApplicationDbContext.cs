1a1a608ab3c5fc6a14a83f0288a91e8cc890b797
Kevin <kevinluch@gmail.com>
2023-03-15 19:52:56 -0500

    edited files

--- Web_App_Patient/Data/ApplicationDbContext.cs
+++ Web_App_Patient/Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace Web_App_Patient.Data
 {
     public class ApplicationDbContext : DbContext 
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
         public DbSet<Patient> Patient { get; set; }
     }
 }
---
+++ Web_App_Patient/Migrations/20230316004652_AddPatientToDb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_App_Patient.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPatientToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patient",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ZipCode = table.Column<int>(type: "int", nullable: false),
+                    AdmitTo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patient", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patient");
+        }
+    }
+}
